b04439629bdca393c2449e8aaadaba351de9845a
Chris Swanner and Joel Adams <chris@>
2018-04-02 16:45:09 -0700

    add everything

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import * as firebase from 'firebase';
+import * as $ from 'jquery';
+
+interface Post {
+  title: string;
+  content: string;
+  picture: File;
+}
+
+interface PostId extends Post {
+  id: string;
+}
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  title:string;
+  content:string;
+  // picture:File;
+  postDoc: AngularFirestoreDocument<Post>;
+  singlePost: Observable<Post>;
+
+
+  postsCol: AngularFirestoreCollection<Post>;
+  posts: any;
+  constructor(private afs: AngularFirestore) {}
+
+  ngOnInit() {
+    this.postsCol = this.afs.collection('posts');
+    this.posts = this.postsCol.snapshotChanges().map(actions => {
+      return actions.map(a => {
+        const data = a.payload.doc.data() as Post;
+        const id = a.payload.doc.id;
+        console.log(id);
+        return { id, data };
+      });
+    });
+  }
+
+  addPost() {
+    let picture = $('#picture').get(0).files[0];
+    this.afs.collection('posts').add({'title': this.title, 'content': this.content, picture});
+
+  }
+
+  getPost(postId) {
+    this.postDoc = this.afs.doc('posts/' + postId);
+    this.singlePost = this.postDoc.valueChanges();
+    console.log(postId)
+  }
+
+  uploadImage(image: string, postId: string): any {
+    let storageRef = firebase.storage().ref();
+    let imageRef = storageRef.child(`${postId}.jpg`);
+  }
+
+  deletePost(postId) {
+    this.afs.doc('posts/' + postId).delete();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 
+var firebaseConfig = {
+  apiKey: "AIzaSyDswAymP2lHroh-ST5lR9ppF1zhPoQU_oo",
+  authDomain: "firestore-30e91.firebaseapp.com",
+  databaseURL: "https://firestore-30e91.firebaseio.com",
+  projectId: "firestore-30e91",
+  storageBucket: "",
+  messagingSenderId: "516474562746"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
